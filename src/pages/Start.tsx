037e05cd470f7fc5ee2db371aee84fcd28204f7a
Piotr <Piotrko64@gmail.com>
2021-09-23 21:39:02 +0200

    title

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -4,25 +4,7 @@ import back1 from '../img/background.jpg';
 import arleft from '../img/leftar.webp';
 import arright from '../img/rightar.webp';
 import arrow from '../img/arrow.png';
-import { Swiper, SwiperSlide } from '../../node_modules/swiper/react';
-// Import Swiper styles
 
-import 'swiper/swiper-bundle.min.css'
-
-// swiper core styles
-import 'swiper/swiper.min.css'
-
-// modules styles
-import 'swiper/components/navigation/navigation.min.css'
-import 'swiper/components/pagination/pagination.min.css'
-
-// import Swiper core and required modules
-import SwiperCore, {
-    Pagination,Navigation
-  } from 'swiper';
-  
-  // install Swiper modules
-  SwiperCore.use([Pagination,Navigation]);
 const Start = () => {
     useEffect(() => {
         const up = document.querySelectorAll('.Start__sectiontitle');

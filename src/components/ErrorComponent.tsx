a658e849a2b395c375d78842e75137bd0f94929c
Piotr <Piotrko64@gmail.com>
2021-10-08 18:44:17 +0200

    add error component

---
+++ src/components/ErrorComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/ErrorComponent.scss'
+
+function ErrorComponent() {
+  return (
+    <div className="error">
+      Page not found
+    </div>
+  );
+}
+
+export default ErrorComponent;

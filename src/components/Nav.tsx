d4f5dc2383300f598939b607e7e132a522f0fff2
Piotr <Piotrko64@gmail.com>
2021-10-08 16:27:19 +0200

    nothing

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -87,9 +87,9 @@ btnline.forEach(line=>{
             </div>
          <ul className="nav__ul">
             
-            <li><NavLink to="/Music">Music</NavLink></li>
-            <li><NavLink to="/SoundsEffects">Sounds Effects</NavLink></li>
-            <li><NavLink to="/Ambience">Ambience</NavLink></li> 
+            <li><NavLink to="/Music" exact >Music</NavLink></li>
+            <li><NavLink to="/SoundsEffects" exact>Sounds Effects</NavLink></li>
+            <li><NavLink to="/Ambience" exact>Ambience</NavLink></li> 
             <div className="nav__hiperplus"> 
             <a href="https://www.youtube.com/channel/UCseRS2xV0cIl4Mm44b4rqvw" target="_blank"><img src={ytlogo}/></a>
             <br></br>

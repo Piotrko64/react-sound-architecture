7e2dd5dcbdbd5a5e7364fa563cb87cf4ccb4f700
Piotrko64 <PiotrPc77@gmail.com>
2022-02-03 11:56:54 +0100

    react helmet

--- src/pages/Band.tsx
+++ src/pages/Band.tsx
@@ -1,12 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 const Band = ({ value }: { value: number }) => {
-
-
     let [newAmb, setNewAmb] = useState<any[]>([
         {
             iframe: "https://bandcamp.com/EmbeddedPlayer/album=2608927152/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/",
             href: "https://soundarchitecture.bandcamp.com/album/windy-marina-ambience",
-            title: "windy marina ambience"
+            title: "windy marina ambience",
         },
         {
             iframe: "https://bandcamp.com/EmbeddedPlayer/album=1887217169/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/",
@@ -16,24 +14,30 @@ const Band = ({ value }: { value: number }) => {
         {
             iframe: "https://bandcamp.com/EmbeddedPlayer/album=714654310/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/",
             href: "https://soundarchitecture.bandcamp.com/album/air-conditioner",
-            title: "air conditioner"
-        }
+            title: "air conditioner",
+        },
     ]);
     useEffect(() => {
         // API AMB
-        const apisounds: string = 'https://apiforsa.herokuapp.com/read/onlySE';
+        const apisounds: string = "https://apiforsa.herokuapp.com/read/onlySE";
         fetch(apisounds)
-            .then(response => response.json())
-            .then(data => {
-                setNewAmb(data.reverse())
-                console.log(data);
-                console.log(newAmb[0].iframe.toString())
+            .then((response) => response.json())
+            .then((data) => {
+                setNewAmb(data.reverse());
             });
     }, []);
     return (
         <>
-            <iframe style={{ border: "0" }} loading="lazy" src={newAmb[value].iframe} seamless><a href={newAmb[value].href}> {newAmb[value].title}</a></iframe>
+            <iframe
+                style={{ border: "0" }}
+                loading="lazy"
+                src={newAmb[value].iframe}
+                seamless
+                title={newAmb[value].iframe}
+            >
+                <a href={newAmb[value].href}> {newAmb[value].title}</a>
+            </iframe>
         </>
-    )
-}
+    );
+};
 export default Band;
